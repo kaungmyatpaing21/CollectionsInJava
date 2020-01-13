4117cbbe3527b298c08217776c3c1c162463c672
kaungmyatpaing21 <kaungmyatpaing2110@gmail.com>
2020-01-14 01:52:26 +0630

    Basic Theatre booking seat code

--- src/com/main/collections/Main.java
+++ src/com/main/collections/Main.java
@@ -2,6 +2,17 @@ package com.main.collections;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Theatre theatre = new Theatre("SanPya", 8, 12);
+//        theatre.getSeats();
+        if (theatre.reserveSeat("A01")){
+            System.out.println("Please Pay!");
+        }else{
+            System.out.println("Seat is already reserved.");
+        }
+        if (theatre.reserveSeat("A01")){
+            System.out.println("Please Pay!");
+        }else{
+            System.out.println("Seat is already reserved.");
+        }
     }
 }
